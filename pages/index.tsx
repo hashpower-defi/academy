3b7a9ab47c3d20e78ca996b759401a2791dcd182
hashpower-defi <hashpowerprotocol@gmail.com>
2021-10-25 14:38:10 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -66,6 +66,8 @@ const Home: NextPage = () => {
                     <Header />
                 </section>
 
+                {/* Ha */}
+
                 {/* Title */}
                 <section className="section__main__title" id="title">
                     <div className="">
