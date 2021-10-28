45a716cd12c60df6f4f79c83e879d2ca3bf51b62
hashpower-defi <hashpowerprotocol@gmail.com>
2021-10-28 16:30:40 +0900

    Deploy Next.js to gh-pages

--- _next/static/eY0rJaUM-93ZbjjE1_1Yw/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-a58f182aa1b723b2f9be.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
